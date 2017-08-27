4f1d9d82ee01e9f7e7ac60a8e0d88438f0f44f0d
toricor <irotoridoritoriyaben@yahoo.co.jp>
2017-08-27 21:41:39 +0900

    カスタムフィルタの追加

--- app/Filters.scala
+++
@@ -1,6 +0,0 @@
-import javax.inject.Inject
-
-import play.api.http.DefaultHttpFilters
-import play.filters.csrf.CSRFFilter
-
-class Filters @Inject() (csrfFilter: CSRFFilter) extends DefaultHttpFilters(csrfFilter)
