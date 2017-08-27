b9721961d64279c7b911892c64fb72893de11ba4
toricor <irotoridoritoriyaben@yahoo.co.jp>
2017-08-27 20:56:12 +0900

    add Filters.scala

---
+++ app/Filters.scala
@@ -0,0 +1,6 @@
+import javax.inject.Inject
+
+import play.api.http.DefaultHttpFilters
+import play.filters.csrf.CSRFFilter
+
+class Filters @Inject() (csrfFilter: CSRFFilter) extends DefaultHttpFilters(csrfFilter)
